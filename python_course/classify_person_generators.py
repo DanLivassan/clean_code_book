9f466cb703369923456ed569d4b7c99b3c6d9b44
Danilo Santana <daniloxc@msn.com>
2021-03-05 12:46:04 -0300

    Make a itertor class PersonIterator

--- python_course/classify_person_generators.py
+++ python_course/classify_person_generators.py
@@ -2,15 +2,43 @@ from faker import Faker
 from random import randint
 from dan_util import performance
 
+
 class Person:
     def __init__(self, name, age):
         self.name = name
         self.age = age
 
 
+class PersonIterator:
+    def __init__(self):
+        self.registro = open("persons.csv", "r")
+        self.actual_line = ""
+        self._actual_person = None
+
+    @staticmethod
+    def make_person(**kwargs):
+        name = kwargs['name'].strip()
+        age = int(kwargs['age'])
+        return Person(name, age)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self.actual_line = self.registro.readline()
+        if self.actual_line == "name, age\n":
+            self.actual_line = self.registro.readline()
+        if not self.actual_line == "":
+            name, age = self.actual_line.split(",")
+            self.actual_person = self.make_person(name=name, age=age)
+            return self.actual_person
+        raise StopIteration
+
+
 def can_enlist_filter(max_age: int):
     def can_enlist_for_army(person: Person) -> bool:
         return person.age <= max_age
+
     return can_enlist_for_army
 
 
@@ -50,16 +78,11 @@ def generate_persons_by_csv_with_generator():
 #generate_dummy_persons_csv(1000000)
 
 
-def generate_classified_persons():
-    persons = generate_persons_by_csv_with_generator()
-    my_filter = can_enlist_filter(30)
-    candidates = filter(my_filter, persons)
-    return candidates
-
-
 @performance
 def printout():
     print([candidate.name for candidate in generate_classified_persons()])
 
 
-printout()
+persons = PersonIterator()
+print([person for person in persons])
+
